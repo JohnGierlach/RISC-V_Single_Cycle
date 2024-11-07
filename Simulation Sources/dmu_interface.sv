4756965d0934ded0521a96221561cf287f5e241d
John Gierlach <jgierlach17@gmail.com>
2024-11-07 15:06:16 -0500

    Setup interfaces for reg file, DMU, and ALU

---
+++ Simulation Sources/dmu_interface.sv
@@ -0,0 +1,12 @@
+interface alu_interface(input logic clock);
+  
+    logic clk,
+    logic rst,
+    logic read_en,
+    logic write_en,
+    logic[WIDTH-1:0] write_data,
+    logic[WIDTH-1:0] addr,
+    logic[WIDTH-1:0] out_data
+  
+  
+endinterface: alu_interface
