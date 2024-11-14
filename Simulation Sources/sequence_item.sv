b52fda78e8bab398f2f076eb0f2876347340c0e5
John Gierlach <jgierlach17@gmail.com>
2024-11-13 23:25:22 -0500

    Setup sequence items ALU, DMU, and Register file

--- Simulation Sources/sequence_item.sv
+++ Simulation Sources/sequence_item.sv
@@ -27,3 +27,58 @@ class alu_sequence_item extends uvm_sequence_item;
   endfunction: new
 	
 endclass
+
+
+class rf_sequence_item extends uvm_sequence_item;
+	
+  `uvm_object_utils(rf_sequence_item)
+  
+  // Instantiations
+  
+  // Inputs
+  rand logic reset;
+  rand logic[7:0] a, b;
+  rand logic[3:0] op_code;
+  
+  // Default Constraints
+  constraint input1_c{a inside {[10:20]};}
+  constraint input2_c{b inside {[1:10]};}
+  constraint op_code_c{op_code inside {[0:3]};}
+  
+  // Outputs
+  logic[7:0] result;
+  bit carryout;
+  
+  
+  function new(string name = "rf_sequence_item");
+    super.new(name);
+  endfunction: new
+	
+endclass
+
+class dmu_sequence_item extends uvm_sequence_item;
+	
+  `uvm_object_utils(dmu_sequence_item)
+  
+  // Instantiations
+  
+  // Inputs
+  rand logic reset;
+  rand logic[7:0] a, b;
+  rand logic[3:0] op_code;
+  
+  // Default Constraints
+  constraint input1_c{a inside {[10:20]};}
+  constraint input2_c{b inside {[1:10]};}
+  constraint op_code_c{op_code inside {[0:3]};}
+  
+  // Outputs
+  logic[7:0] result;
+  bit carryout;
+  
+  
+  function new(string name = "dmu_sequence_item");
+    super.new(name);
+  endfunction: new
+	
+endclass
