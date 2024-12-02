810ba54745623d6249ee62cf80ed8e74c77d41d0
John Gierlach <jgierlach17@gmail.com>
2024-12-02 16:17:16 -0500

    Fixed syntax errors in interfaces

--- Simulation Sources/dmu_interface.sv
+++ Simulation Sources/dmu_interface.sv
@@ -1,12 +1,12 @@
-interface dmu_interface(input logic clock);
+interface dmu_interface#(parameter WIDTH = 32)(input logic clock);
   
-    logic clk,
-    logic rst,
-    logic read_en,
-    logic write_en,
-    logic[WIDTH-1:0] write_data,
-    logic[WIDTH-1:0] addr,
-    logic[WIDTH-1:0] out_data
+    logic clk;
+    logic rst;
+    logic read_en;
+    logic write_en;
+    logic[WIDTH-1:0] write_data;
+    logic[WIDTH-1:0] addr;
+    logic[WIDTH-1:0] out_data;
   
   
 endinterface: dmu_interface
--- Simulation Sources/rf_interface.sv
+++ Simulation Sources/rf_interface.sv
@@ -1,9 +1,9 @@
-interface rf_interface(input logic clock);
+interface rf_interface#(parameter WIDTH = 32)(input logic clock);
   
-    logic clk, rst, write_en,
-    logic[4:0] RS1, RS2, RD,
-    logic[WIDTH-1:0] RD_data,
-    logic[WIDTH-1:0] RS1_data, RS2_data
+    logic clk, rst, write_en;
+    logic[4:0] RS1, RS2, RD;
+    logic[WIDTH-1:0] RD_data;
+    logic[WIDTH-1:0] RS1_data, RS2_data;
   
   
 endinterface: rf_interface
