599905b2cee8bca16192f303f04535a27b8c5c9e
John Gierlach <jgierlach17@gmail.com>
2024-12-04 19:05:47 -0500

    Fixed bug, off

--- Simulation Sources/data_tx_interface.sv
+++ Simulation Sources/data_tx_interface.sv
@@ -9,7 +9,7 @@ interface data_tx_interface#(parameter WIDTH = 32)(input logic clock);
     logic [11:0] Imm_reg;
     logic [4:0] Shamt;
     logic  write_en; read_en;
-    logic [4:0] RS1; RS2; RD;
+    logic [4:0] RS1, RS2, RD;
     logic [WIDTH-1:0] ALU_data_out;
     logic [WIDTH-1:0] Mem_addr_out;
     logic [WIDTH-1:0] RS2_data_out; RS1_data_out;
